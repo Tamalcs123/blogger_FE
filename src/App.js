59bf7a3cb6f010894d427d8de372c143408a5d2f
Tamal Naskar <87684531+Tamalcs123@users.noreply.github.com>
2023-12-25 21:00:59 +0530

    Update App.js
    Add protected and private route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 import Blogs from "./pages/Blogs";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -10,6 +10,8 @@ import NotFound from "./pages/NotFound";
 import { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
 import { CircularProgress } from "@mui/material";
+import PublicRoute from "./components/PublicRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -24,40 +26,91 @@ function App() {
 
   return (
     <>
-      {loading && (
-        <div
-          className=""
-          role="status"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            position: "fixed",
-            top: "0",
-            left: "0",
-            width: "100%",
-            height: "100%",
-            zIndex: "9999",
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-          }}
-        >
-          <div style={{ height: "5rem", width: "5rem", color: "white" }}>
-            <CircularProgress size={70} style={{ color: "#1976D2" }} />
+      
+        {loading && (
+          <div
+            className=""
+            role="status"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              position: "fixed",
+              top: "0",
+              left: "0",
+              width: "100%",
+              height: "100%",
+              zIndex: "9999",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+            }}
+          >
+            <div style={{ height: "5rem", width: "5rem", color: "white" }}>
+              <CircularProgress size={70} style={{ color: "#1976D2" }} />
+            </div>
           </div>
-        </div>
-      )}
-      <Header />
-      <Toaster />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Blogs />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/my-blogs" element={<UserBlogs />} />
-        <Route path="/blog-details/:id" element={<BlogDetails />} />
-        <Route path="/create-blog" element={<CreateBlog />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+        )}
+        <Header />
+        <Toaster />
+        <Routes>
+          <Route
+            path="/login"
+            element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Blogs />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/blogs"
+            element={
+              <ProtectedRoute>
+                <Blogs />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/my-blogs"
+            element={
+              <ProtectedRoute>
+                <UserBlogs />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/blog-details/:id"
+            element={
+              <ProtectedRoute>
+                <BlogDetails />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/create-blog"
+            element={
+              <ProtectedRoute>
+                <CreateBlog />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+     
     </>
   );
 }
